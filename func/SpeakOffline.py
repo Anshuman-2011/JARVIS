2b50b50f3fac165121b28d95aa307ae8baf252ca
Divy0The0Fire <104820663+Divy0The0Fire@users.noreply.github.com>
2023-11-30 16:13:25 +0530

    JARVIS

---
+++ func/SpeakOffline.py
@@ -0,0 +1,17 @@
+#pip install pyttsx3
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('rate', 200)
+engine.setProperty('voice', voices[0].id)
+
+
+def Speak(*args, **kwargs):
+    audio = ""
+    for i in args:
+        audio += str(i)
+    print(audio)
+    engine.say(audio)
+    engine.runAndWait()

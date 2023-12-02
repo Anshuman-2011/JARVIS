d4e7db35f2d52caa4db2028841c1ae255cf9a30a
Divy0The0Fire <104820663+Divy0The0Fire@users.noreply.github.com>
2023-12-03 04:20:54 +0530

    JARVIS

--- README.md
+++
@@ -1,4 +0,0 @@
-# JARVIS
-
-please install the model in data folder using this link 
-https://drive.google.com/file/d/1IpuhzGJt54MnIjiooyjengej_qNpYiNl/view?usp=sharing
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from func.SpeakOffline import Speak
 from func.ListenJs import Listen
 from func.DataOnline import Online_Scraper
 from llm.ChatGpt import ChatGpt
+from func.Ocr import Ocr
 
 if __name__=="__main__":
     while 1:
@@ -12,9 +13,23 @@ if __name__=="__main__":
         SQ=Q.split(" ")[0]           
         EQ=Q.split(" ")[-1]
         
+        if (SQ=="click" or (SQ=="double" and "click" in Q)) and LQ<7:
+            QL.replace("click","")
+            QL.replace("on","")
+            QL.replace("jarvis","")
+            QL.replace("double","")
+            A=Ocr(QL.strip())
+            Speak(A)
+        else :
+            web=Online_Scraper(Q)
+            if web!=None:
+                Speak(web)
+            elif Chat(QL)[1]>0.99:
+                Speak(Chat(QL)[0])
+            else:
+                reply=ChatGpt(Q," ***reply like tony stark jarvis in less words***")
+                Speak(reply)
 
-        Speak(GhatGpt(Q))
 
-    
-        
+
 

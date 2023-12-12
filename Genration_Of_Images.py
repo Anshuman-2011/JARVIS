91fd7ea802059030a39baad31150b50ad227d8f7
Divy0The0Fire <104820663+Divy0The0Fire@users.noreply.github.com>
2023-12-12 06:10:51 +0530

    JARVIS

--- Genration_Of_Images.py
+++ Genration_Of_Images.py
@@ -2,7 +2,7 @@
 from os import system , listdir
 from PIL import Image
 
-C=COKKEE #WATCH THIS IF YOU DON"T HAVE COKKEE OR YOU CAN PUT "0" as your cokkee https://www.youtube.com/watch?v=_PL7UMzaSBk 
+C=COKKEE ##WATCH THIS IF YOU DON"T HAVE COKKEE OR YOU CAN PUT "0" as your cokkee https://www.youtube.com/watch?v=_PL7UMzaSBk 
 def Generate_Images(prompt:str):
     system(f'python -m BingImageCreator --prompt "{prompt}" -U {C}')
     return listdir("output")[-4:]
